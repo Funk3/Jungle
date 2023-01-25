39030da73eed1e3e984c35c925784169c4f17ee9
Zak <zak.johansson@gmail.com>
2023-01-25 15:16:31 -0800

    add dashboard counts feature

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,9 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
   
   def show
+    @product_count = Product.all.count
+    @evergreens_count = Product.where(category_id: 1).count
+    @shrubs_count = Product.where(category_id: 2).count
+    @trees_count = Product.where(category_id: 3).count
   end
 end
